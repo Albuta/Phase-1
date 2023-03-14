45900d76f075e4cf79a08610ff0dc19bda80b4a0
Giorgi Albutashvili <127154107+Albuta@users.noreply.github.com>
2023-03-15 01:25:13 +0400

    Deep Thought

---
+++ deep.py
@@ -0,0 +1,5 @@
+answr = input("what is the answer to the Great Question of Life, the Universe and Everything? ").lower()
+if answr == "42" or answr == "forty-two" or answr == "forty two":
+    print("YES")
+else:
+    print("NO")
